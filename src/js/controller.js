ec96a581f39c506ef7397193bb24a3e301dc7a01
Bhavani Bolloju <bhavani.bolloju@gmail.com>
2023-12-06 12:05:46 +0530

    implement search results functionality

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,8 @@
 import * as model from "./model.js";
 import recipeView from "./view/recipeView.js";
+import searchView from "./view/searchView.js";
+import resultsView from "./view/resultsView.js";
+
 import icons from "../img/icons.svg";
 // import "core.js/stable";
 import "regenerator-runtime/runtime";
@@ -8,8 +11,13 @@ import "regenerator-runtime/runtime";
 
 ///////////////////////////////////////
 
+if (module.hot) {
+  module.hot.accept();
+}
+
 const controlRecipes = async function () {
   const id = window.location.hash.slice(1);
+  // console.log(id);
 
   if (!id) return;
 
@@ -25,8 +33,34 @@ const controlRecipes = async function () {
   }
 };
 
-const init = function () {
-  recipeView.handlerRender(controlRecipes);
+const controlSearchResults = async function () {
+  //read query
+  const query = searchView.getQuery();
+
+  if (!query) return;
+  try {
+    //load search results
+    resultsView.spinner();
+    await model.loadSearchResults(query);
+    //render search results
+
+    const result = model.state.search.results.slice(0, 10);
+
+    if (result.length <= 0) {
+      throw new Error("No results found, try with different keyword");
+    }
+    resultsView.render(result);
+    //clear query input
+    searchView.clearSearchInput();
+  } catch (error) {
+    resultsView.renderError(error.message);
+  }
 };
 
+// controlSearchResults();
+
+const init = function () {
+  recipeView.handlerRender(controlRecipes);
+  searchView.handlerSearchResult(controlSearchResults);
+};
 init();
